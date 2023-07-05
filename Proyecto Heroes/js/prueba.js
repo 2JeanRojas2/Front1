cee070c7e809b62464163ccafff7309460e26e63
jeanrojas911@gmail.com <86921417+2JeanRojas2@users.noreply.github.com>
2023-07-05 12:27:52 -0500

    Proyectos 2 meses Html, Css, JavaScript Basico

---
+++ Proyecto Heroes/js/prueba.js
@@ -0,0 +1,11 @@
+let jugador1 = 10;
+let jugador2 = 5;
+let contador = 0;
+
+if (jugador1 < jugador2) {
+    contador++;
+    console.log(contador);
+} else if (jugador1 > jugador2) {
+    contador--;
+    console.log(contador);
+}
